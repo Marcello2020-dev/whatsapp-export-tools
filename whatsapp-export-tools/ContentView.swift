91fc618596c279faf6becd1db4c58216246459e5
Marcello2020-dev <84258145+Marcello2020-dev@users.noreply.github.com>
2026-01-04 12:59:50 +0100

    Add SwiftUI app files and WhatsApp export script

---
+++ whatsapp-export-tools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whatsapp-export-tools
+//
+//  Created by Marcel Mißbach on 04.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whatsapp-export-tools/whatsapp_export_toolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsapp_export_toolsApp.swift
+//  whatsapp-export-tools
+//
+//  Created by Marcel Mißbach on 04.01.26.
+//
+
+import SwiftUI
+
+@main
+struct whatsapp_export_toolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
